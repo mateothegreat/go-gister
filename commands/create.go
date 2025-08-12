c1856dbea46702aec98787c64873d8ff1785bc8e
Matthew Davis <matthew@matthewdavis.io>
2025-08-12 07:30:09 -0500

    bump

--- commands/create.go
+++ commands/create.go
@@ -6,12 +6,15 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
-	"github.com/google/go-github/v63/github"
+	"github.com/google/go-github/v74/github"
 	"github.com/mateothegreat/go-multilog/multilog"
+	"github.com/mateothegreat/go-util/paths"
 	"github.com/spf13/cobra"
 )
 
+// File represents a file in a GitHub Gist.
 type File struct {
 	Path      string
 	Filename  string
@@ -19,11 +22,35 @@ type File struct {
 	Content   string
 }
 
+func init() {
+	multilog.RegisterLogger(multilog.LogMethod("console"), multilog.NewConsoleLogger(&multilog.NewConsoleLoggerArgs{
+		Level:  multilog.DEBUG,
+		Format: multilog.FormatText,
+	}))
+
+	Create.Flags().StringP("username", "u", "", "GitHub username.")
+	Create.MarkFlagRequired("username")
+
+	Create.Flags().StringP("token", "t", "", "GitHub token.")
+	envToken := os.Getenv("GITHUB_TOKEN")
+	if envToken == "" {
+		Create.MarkFlagRequired("token")
+	}
+
+	Create.Flags().StringP("description", "d", "", "Gist description.")
+	Create.MarkFlagRequired("description")
+
+	Create.Flags().StringP("path", "", "", "Path to the file or directory to gist.")
+	Create.MarkFlagRequired("path")
+
+	Create.Flags().BoolP("public", "p", false, "Make the gist public.")
+}
+
+// Create a new GitHub Gist.
 var Create = &cobra.Command{
 	Use:   "create",
 	Short: "Create a new GitHub Gist.",
-	Long:  "Create a new GitHub Gist.",
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		username, err := cmd.Flags().GetString("username")
 		if err != nil {
 			multilog.Fatal("create", "failed to get username", map[string]interface{}{
@@ -31,11 +58,17 @@ var Create = &cobra.Command{
 			})
 		}
 
-		token, err := cmd.Flags().GetString("token")
-		if err != nil {
-			multilog.Fatal("create", "failed to get token", map[string]interface{}{
-				"error": err,
-			})
+		var token string
+		envToken := os.Getenv("GITHUB_TOKEN")
+		if envToken != "" {
+			token = envToken
+		} else {
+			token, err = cmd.Flags().GetString("token")
+			if err != nil {
+				multilog.Fatal("create", "failed to get token", map[string]interface{}{
+					"error": err,
+				})
+			}
 		}
 
 		public, err := cmd.Flags().GetBool("public")
@@ -52,15 +85,22 @@ var Create = &cobra.Command{
 			})
 		}
 
-		path, err := cmd.Flags().GetString("path")
+		originalPath, err := cmd.Flags().GetString("path")
 		if err != nil {
 			multilog.Fatal("create", "failed to get path", map[string]interface{}{
 				"error": err,
 			})
 		}
 
+		expandedPath, err := paths.Expand(originalPath)
+		if err != nil {
+			multilog.Fatal("create", "failed to expand path", map[string]interface{}{
+				"error": err,
+			})
+		}
+
 		files := make([]File, 0)
-		err = filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		err = filepath.Walk(expandedPath, func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
@@ -70,9 +110,13 @@ var Create = &cobra.Command{
 				if err != nil {
 					return err
 				}
+				multilog.Debug("create", "file found", map[string]interface{}{
+					"path": path,
+					"size": info.Size(),
+				})
 				files = append(files, File{
 					Path:     path,
-					Filename: filepath.Base(path),
+					Filename: strings.ReplaceAll(strings.Replace(path, expandedPath+"/", "", 1), "/", "__"),
 					Content:  string(content),
 					Directory: func() string {
 						dir := filepath.Dir(path)
@@ -118,25 +162,4 @@ var Create = &cobra.Command{
 			"files": len(gistFiles),
 		})
 	},
-}
-
-func init() {
-	multilog.RegisterLogger(multilog.LogMethod("console"), multilog.NewConsoleLogger(&multilog.NewConsoleLoggerArgs{
-		Level:  multilog.DEBUG,
-		Format: multilog.FormatText,
-	}))
-
-	Create.Flags().StringP("username", "u", "", "GitHub username.")
-	Create.MarkFlagRequired("username")
-
-	Create.Flags().StringP("token", "t", "", "GitHub token.")
-	Create.MarkFlagRequired("token")
-
-	Create.Flags().StringP("description", "d", "", "Gist description.")
-	Create.MarkFlagRequired("description")
-
-	Create.Flags().StringP("path", "", "", "Path to the file or directory to gist.")
-	Create.MarkFlagRequired("path")
-
-	Create.Flags().BoolP("public", "p", false, "Make the gist public.")
 }
